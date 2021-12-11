44a07b8ad33b8ccbb066266e7912e18c8b1f4525
hubi94 <anja.hubac@gmail.com>
2021-12-11 15:45:34 +0100

    make navbar responsive and add unfinished form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+
+
 (function($) {
 
 	let	$window = $(window),
@@ -17,6 +19,8 @@
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
 			}, 100);
+
+			cart();
 		});
 
 	// Scrolly.
@@ -24,5 +28,64 @@
 			speed: 1000,
 			offset: function() { return $nav.height(); }
 		});
+
+
+	//Responsive NavBar
+
+		function hamburger(){
+			let x = document.getElementsByClassName('ul');
+			if (x.className === 'container') {
+				x.className += 'responsive'; 
+			} else {
+				x.className = 'container';
+			};
+		}
+
+	
+		// SIGN UP/LOG IN FORM
+
+	$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
 
 })(jQuery);
